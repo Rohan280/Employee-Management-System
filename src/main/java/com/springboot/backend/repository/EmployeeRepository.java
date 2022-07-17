cc5ced1edee169d93442baf890f5e3e14ac22c96
Rohan Singhal <singhalr280@gmail.com>
2022-07-17 09:54:06 +0530

    First Commit

---
+++ src/main/java/com/springboot/backend/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.backend.repository;
+
+import com.springboot.backend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/main/java/com/springboot/backend/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,57 @@
+package com.springboot.backend.service.impl;
+
+import com.springboot.backend.exception.ResourceNotFoundException;
+import com.springboot.backend.model.Employee;
+import com.springboot.backend.repository.EmployeeRepository;
+import com.springboot.backend.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @Override
+    public List<Employee> getAllEmployee() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        return employeeRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Employee", "Id", id));
+    }
+
+    @Override
+    public Employee updateEmployee(Employee employee, long id) {
+        Employee existingEmployee = employeeRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Employee", "Id", id));
+
+        existingEmployee.setFirstName(employee.getFirstName());
+        existingEmployee.setLastName(employee.getLastName());
+        existingEmployee.setEmail(employee.getEmail());
+
+        employeeRepository.save(existingEmployee);
+
+        return existingEmployee;
+    }
+
+    @Override
+    public void deleteEmployee(long id) {
+        employeeRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Employee", "Id", id));
+        employeeRepository.deleteById(id);
+    }
+}
